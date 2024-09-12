7751670f1e1a658df6edb2c0ba934bafc70670d7
Dimakatso <dimakatsomm@icloud.com>
2024-09-12 19:30:43 +0200

    Fix typo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,5 +4,5 @@ import { env } from 'process';
 export const PORT: number = Number(env.PORT || '3000');
 export const MONGOURI: string = env.MONGOURI || '';
 export const REDIS_PASSWORD: string = env.REDIS_PASSWORD || '';
-export const REDIS_HOST: string = env.REDIS_HOST || 'http://127.0.0.1';
+export const REDIS_HOST: string = env.REDIS_HOST || '127.0.0.1';
 export const REDIS_PORT: number = Number(env.REDIS_PORT || '6380');
