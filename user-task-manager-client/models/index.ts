2a96189802a056ca2b89a80f0ecf9d2ca3bdd43c
Dimakatso <dimakatsomm@icloud.com>
2024-09-12 20:14:06 +0200

    Add client

---
+++ user-task-manager-client/models/index.ts
@@ -0,0 +1,7 @@
+export * from './create-task-dto';
+export * from './create-user-dto';
+export * from './error-dto';
+export * from './task';
+export * from './update-task-dto';
+export * from './update-user-dto';
+export * from './user';
